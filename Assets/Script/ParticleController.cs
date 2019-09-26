9c84c746130d4f8edb9399a365c4a3696973dec7
DominikCG <44614079+DominikCG@users.noreply.github.com>
2019-09-26 10:55:38 -0300

    Add files via upload

---
+++ Assets/Script/ParticleController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+    public GameObject ParticlePf;
+    Vector3 ParticleOrientation = new Vector3(-90, 0, 0);
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject Particle = Instantiate(ParticlePf,collision.contacts[0].point,Quaternion.Euler(ParticleOrientation));
+
+        Destroy(Particle, 2);
+
+        ParticleSystem ps = Particle.GetComponent<ParticleSystem>();
+        ParticleSystem.EmissionModule emissionModule = ps.emission;
+
+        float collisionIntensity = collision.relativeVelocity.magnitude * 100;
+        if(collisionIntensity > 1000){
+            collisionIntensity = 1000;
+        }
+
+        emissionModule.rateOverTime = collisionIntensity;
+
+    }
+
+    
+}
---
+++ Assets/Script/multiFireBalls.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class multiFireBalls : MonoBehaviour
+{
+
+   
+    public GameObject fireBall;
+    private GameObject newFireBall;
+    private Vector3 point;
+    bool creation = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        point = transform.position;
+
+        if (GameObject.FindGameObjectsWithTag("FireBall").Length < 4)
+        {
+            creation = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (creation)
+        { 
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Vector3 newPos;
+                newFireBall = Instantiate(fireBall, point, Quaternion.identity);
+                newFireBall.transform.localScale = 0.5f * newFireBall.transform.localScale;
+                newPos = newFireBall.transform.position;
+                newPos.x += 1;
+                newFireBall.transform.position = newPos;
+
+                newFireBall = Instantiate(fireBall, point, Quaternion.identity);
+                newFireBall.transform.localScale = 0.5f * newFireBall.transform.localScale;
+                newPos = newFireBall.transform.position;
+                newPos.x -= 1;
+                newFireBall.transform.position = newPos;
+
+                Destroy(gameObject);
+
+            }
+        }
+    }
+}
